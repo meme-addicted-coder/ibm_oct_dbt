1f8a719c5c41e568617d366b38e1ae2fccb37e45
meme-addicted-coder <duttaroybhabnashre28@gmail.com>
2025-11-11 15:24:38 +0530

    commit_01

---
+++ target/compiled/mkmall/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from analytics.sourcedb.mk_mall.my_first_dbt_model
+where id is null
+
+
---
+++ target/compiled/mkmall/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from analytics.sourcedb.mk_mall.my_second_dbt_model
+where id is null
+
+
